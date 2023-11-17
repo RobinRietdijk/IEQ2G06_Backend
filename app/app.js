b14b0e67ba6fa07ba3ff23693ce79945c4640571
Robin Rietdijk <robinrietdijk@kabelfoon.net>
2023-11-17 18:14:30 +0100

    Add socket object

--- app/app.js
+++ app/app.js
@@ -1,21 +1,22 @@
 import express from 'express';
 import cors from 'cors';
 import { createServer } from 'http';
-import socketio from './socket.js';
+import SocketManager from './socket.js';
 import routes from './routes/index.js';
+import Socket from './socket.js';
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 const httpServer = createServer(app);
-const io = socketio(httpServer)
+const io = new SocketManager(httpServer);
 
 app.use(cors({
     origin: '*'
 }));
 app.use(express.json());
 app.use(routes.home());
-app.use('/gpt', routes.gpt(io));
-app.set('socket', io);
+app.use('/gpt', routes.gpt(io.io));
+app.set('socket', io.io);
 
 httpServer.listen(PORT, () => {
     console.debug(`Listening on port: ${PORT}`)
--- app/socket.js
+++ app/socket.js
@@ -1,32 +1,38 @@
 import { Server } from "socket.io"
 
-function isTokenInUse(token) {
-    return token;
+const DEFAULT_OPTIONS = {
+    connectionStateRecovery: {
+        maxDisconnectionDuration: 2 * 60 * 1000,
+        skipMiddlewares: true,
+    },  
+    cors: {
+        origin: '*',
+    }
+};
+
+class SocketManager {
+    constructor(httpServer, opts=DEFAULT_OPTIONS) {
+        if (!SocketManager.instance) {
+            SocketManager.instance = this;
+            this.io = new Server(httpServer, opts);
+            this.initListeners()
+        }
+
+        return SocketManager.instance;
+    }
+
+    initListeners() {
+        this.io.on('connection', (socket) => {
+            if (socket.recovered) {
+                console.log(`Socket connection recovered`);
+            } else {
+                console.log(`New socket connected`)
+            }
+            socket.on('disconnect', (reason) => {
+                console.debug(`Socket disconnected, reason: ${reason}`);
+            })
+        });
+    }
 }
 
-export default (httpServer) => {
-    const connections = [];
-    const controllers = {};
-    const io = new Server(httpServer, {
-        connectionStateRecovery: {
-            maxDisconnectionDuration: 2 * 60 * 1000,
-            skipMiddlewares: true,
-        },  
-        cors: {
-            origin: '*',
-        }
-    });
-
-    io.on('connection', (socket) => {
-        if (socket.recovered) {
-            console.log(`Socket connection recovered`);
-        } else {
-            console.log(`New socket connected`)
-        }
-        socket.on('disconnect', (reason) => {
-            console.debug(`Socket disconnected, reason: ${reason}`);
-        })
-    });
-
-    return io;
-}
+export default SocketManager;
