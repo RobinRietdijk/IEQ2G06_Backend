01631acf36237deec362bec4db7802e2639b013c
Robin Rietdijk <robinrietdijk@kabelfoon.net>
2023-11-19 18:54:26 +0100

    Add admin middleware

--- src/routes/middleware.js
+++ src/routes/middleware.js
@@ -1,4 +1,11 @@
 export const adminMiddleware = (req, res, next) => {
-    
-    next();
+    const token = req.headers.authorization;
+
+    if (!token) return res.status(401).json({ message: 'Unauthorized - No token provided' });
+    jwt.verify(token, SECRET, (err, decoded) => {
+        if (err) return res.status(401).json({ message: 'Unauthorized - Invalid token' });
+        if (!decoded.isAdmin) return res.status(403).json({ message: 'Forbidden - Admin access required' });
+        req.user = decoded;
+        next();
+    });
 }
