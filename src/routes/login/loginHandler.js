412f4b60a9b5ec2d9314cc75de5c60e9c3f8cf5e
Robin Rietdijk <robinrietdijk@kabelfoon.net>
2023-11-23 16:16:56 +0100

    Remove refreshHandler

--- src/routes/login/loginHandler.js
+++ src/routes/login/loginHandler.js
@@ -1,28 +1,11 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { ADMIN_USER, ADMIN_PASSWD, SECRET, REFRESH_SECRET } from '../../util/constants.js';
-import refreshHandler from './refresh/refreshHandler.js';
 
 const loginHandler = {
-    REFRESH: refreshHandler,
-    GET: (req, res) => {
-        const token = req.headers.authorization;
-
-        if (!token) {
-          return res.status(401).json({ message: 'Unauthorized - Missing token' });
-        }
-      
-        jwt.verify(token, SECRET, (err, decoded) => {
-          if (err) {
-            return res.status(401).json({ message: 'Unauthorized - Invalid token' });
-          }
-      
-          res.status(200).json({ user: req.user });
-        });
-    },
     POST: (req, res) => {
-        const token = req.headers.authorization;
         const { username, password } = req.body;
+        const token = req.headers.authorization;
         if (token) {
             jwt.verify(token, SECRET, (err, decoded) => {
                 if (err) {
@@ -34,7 +17,7 @@ const loginHandler = {
             const token = jwt.sign({ username, isAdmin: true }, SECRET, { expiresIn: '15m' });
             const refreshToken = jwt.sign({ username: username, isAdmin: true }, REFRESH_SECRET, { expiresIn: '7d' });
             
-            res.json({ token: token, refreshToen: refreshToken });
+            res.json({ token: token, refreshToken: refreshToken });
         } else {
             res.status(401).json({ message: 'Invalid username or password' });
         }
