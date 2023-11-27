f76c9b599675ea36548c81b99a86ab73dc4a341b
Robin Rietdijk <robinrietdijk@kabelfoon.net>
2023-11-27 13:58:39 +0100

    Remove unneeded id value

--- src/routes/gpt/gptHandler.js
+++ src/routes/gpt/gptHandler.js
@@ -5,8 +5,7 @@ api = new ChatGPT();
 
 const gptHandler = {
     POST: async (req, res) => {
-        const id = Date.now().toString(36) + Math.random().toString(36).substring(2);
-        const result = await api.sendMessage(id, req.body.message);
+        const result = await api.sendMessage(req.body.message);
         res.status(200).json(result);
     }
 };
