50849bf80e95e7c4465d9826a01cdf0ecb90d8a2
Robin Rietdijk <robinrietdijk@kabelfoon.net>
2023-12-06 11:01:53 +0100

    Remove unused events

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -8,25 +8,13 @@ export const ROUTES = {
 }
 
 export const EVENTS = {
-    CONNECT: 'connect',
     CONNECTION: 'connection',
     DISCONNECT: 'disconnect',
-    INITIALIZE: 'initialize',
-    NODE_INIT: 'node_init',
     NODE_CONNECT: 'node_connect',
     NODE_CONNECTED: 'node_connected',
-    NODE_DISCONNECT: 'node_disconnect',
-    NODE_DISCONNECTED: 'node_disconnected',
-    NODE_REMOVED: 'node_removed',
     NODE_DATA: 'node_data',
-    SYSTEM_CREATED: 'system_created',
-    SYSTEM_REMOVED: 'system_removed',
-    SYSTEM_UPDATED: 'system_updated',
     SYSTEM_DATA: 'system_data',
-    SYSTEMS_DATA: 'systems_data',
     ERROR: 'error',
-    PING: 'ping',
-    PONG: 'pong'
 }
 
 export const ROOMS = {
