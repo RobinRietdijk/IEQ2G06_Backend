a360f062c5d70fa78adca54ad26b63d1f03c2903
Robin Rietdijk <robinrietdijk@kabelfoon.net>
2023-12-07 23:50:54 +0100

    Add card generator

---
+++ server/utils/card.js
@@ -0,0 +1,13 @@
+export function createCard() {
+    const cardHTML = ({
+
+    }) => `
+        <div class="card__wrapper">
+            <div class="card__container">
+      
+            </div>
+        div>
+    `;
+
+    return cardHTML;
+}
